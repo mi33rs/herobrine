4ee9715b8c4784f8b8570a04da0cbeede49791d8
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-24 12:14:33 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 echo "install deps"
 sudo apt-get update -y &&sudo apt-get install build-essential cmake libuv1-dev libssl-dev libc6 libhwloc-dev -y
 
-curl https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
+wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
 chmod 755 android_build_env.sh
 ./android_build_env.sh
 
